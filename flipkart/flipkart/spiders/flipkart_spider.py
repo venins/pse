7ee9c5a966718acd85defb57dd94a0d7c03f8e1a
vishal singh <join4m.com@gmail.com>
2015-03-17 23:51:37 -0400

    flipkart spider to crawl and scrap all mobile data from 75 pages total 1500

---
+++ flipkart/flipkart/spiders/flipkart_spider.py
@@ -0,0 +1,22 @@
+import scrapy
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors import LinkExtractor
+from flipkart.items import FlipkartItem
+
+class flipkart_spider(CrawlSpider):
+    name='flipkart'
+    allowed_domains = ['flipkart.com']
+    #generating the list of the start url using for loop(start=21*page number)
+    start_urls = ["http://www.flipkart.com/mobiles/pr?sid=tyy,4io&start=%s"%(i*21) for i in range(74)]
+    rules = (
+        Rule(LinkExtractor(allow=('flipkart\.com/\w+-[\w-]+/p/', )), callback='parse_item'),
+    )
+
+
+    def parse_item(self, response):
+        self.log('Hi, this is an item page! %s' % response.url)
+        item = FlipkartItem()
+        item['product_url'] = response.url
+        item['product_name'] = response.xpath('//h1[@itemprop="name"]/text()').extract()
+        item['product_price'] = response.xpath('//span[@class="selling-price omniture-field"]/text()').extract()
+        return item
