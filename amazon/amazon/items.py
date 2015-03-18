22477b80149cdde39282d3da4c73d925eb42054d
vishal singh <join4m.com@gmail.com>
2015-03-18 21:09:16 +0530

    all field product_name,product_tag(subname),product_price,product_url,specs,desc define

--- amazon/amazon/items.py
+++ amazon/amazon/items.py
@@ -9,6 +9,11 @@ import scrapy
 
 
 class AmazonItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    product_name=scrapy.Field()
+    #subname of the product like black,white or 8gb , 1gb etc
+    product_tag=scrapy.Field()
+    product_url=scrapy.Field()
+    product_price=scrapy.Field()
+    product_specs=scrapy.Field()
+    product_desc=scrapy.Field()
+    product_pmode=scrapy.Field()
